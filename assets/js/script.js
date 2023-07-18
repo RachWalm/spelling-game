37be64fa2e3fc4083c9db81a8b6544a94b22eb7e
Rachel Walmsley <rachwalm75@gmail.com>
2023-07-18 14:46:07 +0100

    Give each input an id

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("startGame").addEventListener("click", function () {
         console.log("go to start game");
         buildLetters();
-        handleKeys();
         //document.getElementById("instructions").addEventListener("click", function () {
         //console.log("go to instructions");
     });
@@ -23,20 +22,20 @@ let currentWord = wordData[0].word;
 console.log(currentWord);
 
 function buildLetters() {
-    let ltr = "<input class=lettr type=text placeholder=- maxlength=1 onkeydown=handleKeys(event);>";
     console.log("buildLetters activated");
     for (let i = 0; i < currentWord.length; i++) {
+        let ltr = `<input type=text id=input${i} placeholder=- maxlength=1 onkeydown=handleKeys(event);>`;
         document.getElementById("actualGame").innerHTML += ltr;
         console.log(i);
+        console.log(ltr);
     };
 }
 
 function handleKeys(event) {
     //event.preventDefault();
-    //let key = document.getElementsByClassName("lettr");
     if (event.repeat) {
         return false;
-    } else {
+    } else { //need an else if for weird characters
         //if (event === 'keydown') {
         //console.log(event.code);
         console.log(event);
