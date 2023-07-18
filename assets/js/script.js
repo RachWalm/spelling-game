38e0d2b489ed3bf60e84a8fd5489cc2246fdc886
Rachel Walmsley <rachwalm75@gmail.com>
2023-07-18 14:20:31 +0100

    Add record letter typed in function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("startGame").addEventListener("click", function () {
         console.log("go to start game");
         buildLetters();
+        handleKeys();
         //document.getElementById("instructions").addEventListener("click", function () {
         //console.log("go to instructions");
     });
@@ -22,10 +23,28 @@ let currentWord = wordData[0].word;
 console.log(currentWord);
 
 function buildLetters() {
-    let ltr = "<input type=text placeholder=- maxlength=1>";
+    let ltr = "<input class=lettr type=text placeholder=- maxlength=1 onkeydown=handleKeys(event);>";
     console.log("buildLetters activated");
     for (let i = 0; i < currentWord.length; i++) {
         document.getElementById("actualGame").innerHTML += ltr;
         console.log(i);
     };
+}
+
+function handleKeys(event) {
+    //event.preventDefault();
+    //let key = document.getElementsByClassName("lettr");
+    if (event.repeat) {
+        return false;
+    } else {
+        //if (event === 'keydown') {
+        //console.log(event.code);
+        console.log(event);
+        console.log(event.getModifierState("CapsLock")); //NumLock ScrollLock
+        //}
+    }
+}
+
+function userLetter() {
+    console.log('which user letter add to array')
 }
