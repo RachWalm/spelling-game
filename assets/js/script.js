8d7cc6fcc519c91b7ba0c56437f27605f95f3607
Rachel Walmsley <rachwalm75@gmail.com>
2023-07-18 10:38:42 +0100

    Add click - start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
+console.log("connected yah")
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("startGame").addEventListener("click", function () {
+        console.log("go to start game");
+    });
+})
+
+
+
 const wordData = [{
     'word': 'elephant',
     'image': 'url',
-    'decs': 'large animal often found in Africa or Aisa',
+    'decs': 'large animal often found in Africa or Asia',
     'hint': 'f and ph often sound the same'
 }, ]
+
+let currentWord = wordData[0].word;
+console.log(currentWord);
+
+function buildLetters() {
+    //let inputs = currentWord.length;
+    //for (let input of inputs) {ltr.innerHTML +=};
+    let ltr = document.getElementById("actual--game");
+    ltr.innerHTML = "input type=text placeholder=-"
+}
