a9708fcd822bab0fe9394b5efcb3a2cfc5d7dd1a
Rachel Walmsley <rachwalm75@gmail.com>
2023-07-19 08:08:25 +0100

    Close instruction box by button

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ to disable input into later boxes https://www.w3schools.com/jsref/prop_text_disa
 is it all letters https://www.w3resource.com/javascript/form/all-letters-field.php#:~:text=You%20can%20write%20a%20JavaScript,HTML%20form%20contains%20only%20letters.&text=To%20get%20a%20string%20contains,%2F)%20which%20allows%20only%20letters.
 
 modals and dialogues - https://blog.webdevsimplified.com/2023-04/html-dialog/
+
+choose showModal() not just show() so that the rest of the page becomes inert.
--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,20 @@ openButton.addEventListener("click", () => {
 })
 
 closeButton.addEventListener("click", () => {
-    modal.close();
+    modal.close()
 })
 
+dialog.addEventListener("click", e => {
+    const dialogDimensions = dialog.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        dialog.close()
+    }
+})
 
 /**
  * Javascript required to play the game
