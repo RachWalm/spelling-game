a639cce69e1a919b50a6f1095bd9d433aad6623d
Rachel Walmsley <rachwalm75@gmail.com>
2023-07-20 16:06:32 +0100

    Remove wrong letters from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-console.log("connected yah")
+//console.log("connected yah")
 
 /**
  * On DOM load sends it to the game or gives the instructions pop up
@@ -6,7 +6,7 @@ console.log("connected yah")
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("startGame").addEventListener("click", function () {
-        console.log("go to start game");
+        //console.log("go to start game");
         buildLetters();
     });
 })
@@ -51,25 +51,25 @@ const wordData = [{
 }, ]
 
 let currentWord = wordData[0].word;
-console.log(currentWord);
+//console.log(currentWord);
 let guess = [];
 
 function buildLetters() {
-    console.log("buildLetters activated");
+    //console.log("buildLetters activated");
     for (let i = 0; i < currentWord.length; i++) {
         let ltr = `<input type=text id=input${i} placeholder=- maxlength=1;>`;
         document.getElementById("actualGame").innerHTML += ltr;
-        console.log(i);
-        console.log(ltr);
+        //console.log(i);
+        //console.log(ltr);
     };
     firstBox();
 }
 
 function lettersOnly(l) {
     let matcher = /^[A-Za-z]+$/;
-    console.log('letters only in ' + l);
-    console.log('lettersonly input type is' + (typeof (l)));
-    console.log('matcher test is ' + matcher.test(l));
+    //console.log('letters only in ' + l);
+    //console.log('lettersonly input type is' + (typeof (l)));
+    //console.log('matcher test is ' + matcher.test(l));
     return matcher.test(l);
 
 }
@@ -77,19 +77,19 @@ function lettersOnly(l) {
 function whichBoxNumber() {
     let guessLen = parseInt(guess.length);
     let len = guessLen;
-    console.log('box number  equals ' + len);
+    //console.log('box number  equals ' + len);
     return len;
 }
 
 function whichBoxInput() {
     let num = 'input' + whichBoxNumber();
-    console.log('input equals ' + num);
+    //console.log('input equals ' + num);
     return num;
 }
 
 function firstBox() {
     document.getElementById('input0').value.length == 0 ? disableArrayBoxes() : console.log('box full');
-    console.log(document.getElementById('input0').value.length)
+    //console.log(document.getElementById('input0').value.length)
 }
 
 
@@ -112,11 +112,11 @@ function handleKeys(event) {
     //if (event.repeat) {
     //  return false;
     //} else 
-    console.log('output of lettersOnly is ' + lettersOnly(event.key))
+    //console.log('output of lettersOnly is ' + lettersOnly(event.key))
     if (lettersOnly(event.key) === false) {
         alert('you must insert a letter, numbers and special characters are not accepted'); //need an else if for weird characters
     } else if (lettersOnly(event.key) === true) {
-        console.log(event);
+        //console.log(event);
         //console.log(event.getModifierState("CapsLock")); //NumLock ScrollLock
         //console.log(event.key);
         guess.push(event.key);
@@ -124,7 +124,7 @@ function handleKeys(event) {
         console.log('length of guess array is' + guess.length)
         isLetterCorrect();
     } else {
-        console.log('problem in lettersOnly possibly');
+        //console.log('problem in lettersOnly possibly');
     }
 }
 
@@ -133,23 +133,22 @@ function isLetterCorrect() {
     let boxContent = guess[number];
     let actualLetter = currentWord[number];
     let isCorrect = boxContent === actualLetter;
-    console.log('is my letter correct' + isCorrect)
-    console.log('the letter from the current word array is ' + actualLetter);
-    console.log('the box content is ' + boxContent);
+    //console.log('is my letter correct' + isCorrect)
+    //console.log('the letter from the current word array is ' + actualLetter);
+    //console.log('the box content is ' + boxContent);
     if (isCorrect) {
-        console.log('correct letter match')
-        document.getElementById('input0').style.color = "green";
+        //console.log('correct letter match')
+        document.getElementById(whichBoxInput).style.color = "green";
         //disableArrayBoxes();
     } else {
-        console.log('not correct letter match')
-        document.getElementById('input0').style.color = "red";
-        guess = []; //this will need to empty the incorrect answer out of the array
-        //lettersOnly(put character in here);
+        //console.log('not correct letter match')
+        document.getElementById(whichBoxInput).style.color = "red";
+        guess.pop(); //this will need to empty the incorrect answer out of the array
+        console.log(guess);
         //see modifier state in handlekey events
-        //alert('only letters can be accepted not numbers or special characters')
     }
 }
 
-//function showLetter() {
+//function moveOn() {
 //
 //}
